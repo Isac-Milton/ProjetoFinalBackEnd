23dc0e164cbf5b10b114742bb2daf21e41aac65b
Thirza Matos <thirzamatos09@outlook.com>
2026-05-26 21:32:27 -0300

    arrumando detalhes

--- api/LanchoneteAPI/LanchoneteAPI/obj/Release/net10.0/LanchoneteAPI.AssemblyInfo.cs
+++ api/LanchoneteAPI/LanchoneteAPI/obj/Release/net10.0/LanchoneteAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchoneteAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a171c5ca3cc7fdc9271501d094ff82ce05fb211c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchoneteAPI")]
